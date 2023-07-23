91cf2dcfd7014e60bf786ae4482c3b7327b3af81
Riari <3583774+Riari@users.noreply.github.com>
2023-07-23 20:48:52 +0100

    Add button sounds

---
+++ scripts/button-sounds.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@onready var button_sound = $AudioStreamPlayer
+
+func _ready():
+    var buttons = get_tree().get_nodes_in_group("button")
+    for button in buttons:
+        button.connect("pressed", on_button_pressed)
+
+func on_button_pressed():
+    button_sound.play()
